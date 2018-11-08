82fb6b817ac03366748e185add2325161a177800
Nick <nick@theoatmeal.com>
2018-11-08 03:18:52 +0000

    automatically backed up by learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 puts "Hello World!"
+=======
+puts "Hello World!" # Correct this to "Hello World!"
+
+# Your entire program should read:
+# puts "Hello World!"
+>>>>>>> caf7ebb373e7f1942929d1886f15d682531d6b39
